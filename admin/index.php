debd46f2c58ee0e4e42d3fefaf77cc1ab1c12b27
Afzal Hossain <afzal.csedu@gmail.com>
2015-09-12 18:45:52 +0600

    dummy options page added

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
 require_once dirname( __FILE__ ) . '/activate-deactivate.php';
 require_once dirname( __FILE__ ) . '/menu.php';
+//require_once dirname( __FILE__ ) . '/options.php';
 //require_once dirname( __FILE__ ) . '/posttypes/srizon-demo.php';
 //require_once dirname( __FILE__ ) . '/metaboxes/srizon-demo.php';
